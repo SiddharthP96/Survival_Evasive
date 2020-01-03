0bb2827361cd9d609249d8597ab29c73aea3cb31
Siddharth Patwardhan <34710627+SiddharthP96@users.noreply.github.com>
2020-01-03 20:06:14 +0530

    Update trapped.m

--- trapped.m
+++ trapped.m
@@ -1,6 +1,5 @@
 function [ind] = trapped (Z,d,prey,pred)
-%Given that, there is at least one predator in the prey's ngbd, is it
-%trapped?
+%Code to determine whether the prey is trapped at node 'prey', given that there is at least one predator in the prey's ngbd.
 ind=0;
 z=sum(Z(:,pred),2)>0;
 
